038377e86e671c434ea780032ecc368ff8d983ea
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-10-13 21:23:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cravings::Application.config.secret_key_base = '931c54395a7a4e9fb0974b22a7446432160a8a101fc053e1ae56243abd42a707dcc034020fb360552597b5f2a3a02981c069872679d9a75eca0dca42422ffb43'
