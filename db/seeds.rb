8e957a705053f673e108dde00f6b1c5cbb828648
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-10-14 23:46:03 -0700

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-["Eureka!", "YogurtLand", "50-Fifty"].each do |restaurant|
+["Eureka", "YogurtLand", "50-Fifty"].each do |restaurant|
   Restaurant.find_or_create_by_name(restaurant)
 end
 
 [["The Original", 1375],["Napa Burger", 1475], ["Truffle Fries", 9]].each do |food,price|
-  Restaurant.find_by_name("Eureka!").foods.find_or_create_by_name_and_price(food, price)
+  Restaurant.find_by_name("Eureka").foods.find_or_create_by_name_and_price(food, price)
+end
+
+[["Strawberry Froyo", 475],["Red Velvet Froyo", 475], ["Tart Froyo", 475]].each do |food,price|
+  Restaurant.find_by_name("YogurtLand").foods.find_or_create_by_name_and_price(food, price)
+end
+
+[["Spicy Asian Chicken", 1475],["Mandalay Curry", 1565], ["Braised Beef Short Ribs", 1635]].each do |food,price|
+  Restaurant.find_by_name("50-Fifty").foods.find_or_create_by_name_and_price(food, price)
 end
