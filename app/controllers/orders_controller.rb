8fa0b8bae94ad55310f5a273395c1b5c25718e05
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-10-24 09:35:45 -0700

    figuring out why order creation doesn't work with heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,27 +3,27 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # render text: params.inspect
-    @order = Order.new
-    @order.name = params["order"]["name"]
-    @order.delivery_location = params["order"]["delivery_location"]
-    i=0
-    if @order.save!
-      # Cycle through the food items
-      while (params["order"]["food-"+i.to_s+"-name"]) do
+    render text: params.inspect
+    # @order = Order.new
+    # @order.name = params["order"]["name"]
+    # @order.delivery_location = params["order"]["delivery_location"]
+    # i=0
+    # if @order.save!
+    #   # Cycle through the food items
+    #   while (params["order"]["food-"+i.to_s+"-name"]) do
 
-        # Create identical "receipt items" for however many there are 
-        params["order"]["food-"+i.to_s+"-quantity"].to_i.times do 
-          r = ReceiptItem.new
-          r.order = @order
-          r.food = Food.find_by_name(params["order"]["food-"+ i.to_s + "-name"])
-          r.save
-        end
+    #     # Create identical "receipt items" for however many there are 
+    #     params["order"]["food-"+i.to_s+"-quantity"].to_i.times do 
+    #       r = ReceiptItem.new
+    #       r.order = @order
+    #       r.food = Food.find_by_name(params["order"]["food-"+ i.to_s + "-name"])
+    #       r.save
+    #     end
 
-        i = i+1
-      end
-      redirect_to @order
-    end
+    #     i = i+1
+    #   end
+    #   redirect_to @order
+    # end
   
   end
 
